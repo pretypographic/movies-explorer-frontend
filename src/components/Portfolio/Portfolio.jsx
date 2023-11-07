7d151b545b6cf519395af8f57d8e99cf95bbc65c
RO pretypographic <a.pesvi@yandex.ru>
2023-11-07 16:34:48 +0100

    peqaktopu2r about-me

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,37 +1,34 @@
 function Portfolio() {
   return (
-    <>
-      <h1 className="about-me__portfolio-title">Портфолио</h1>
-      <ul className="about-me__portfolio">
-        <li className="about-me__portfolio-work">
+    <article className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__list">
+        <li className="portfolio__item">
           <a
             href="https://pretypographic.github.io/how-to-learn/"
-            className="about-me__link about-me__link_type_text"
+            className="portfolio__link"
             title="Научиться учиться"
             target="_blank"
-            rel="noreferrer">Статичный сайт
-            <span className="about-me__link about-me__link_type_icon">↗</span></a>
+            rel="noreferrer">Статичный сайт</a>
         </li>
-        <li className="about-me__portfolio-work">
+        <li className="portfolio__item">
           <a
             href="https://pretypographic.github.io/russian-travel/"
-            className="about-me__link about-me__link_type_text"
+            className="portfolio__link about-me__link_type_text"
             title="Путешествие по России"
             target="_blank"
-            rel="noreferrer">Адаптивный сайт
-            <span className="about-me__link about-me__link_type_icon">↗</span></a>
+            rel="noreferrer">Адаптивный сайт</a>
         </li>
-        <li className="about-me__portfolio-work">
+        <li className="portfolio__item">
           <a
             href="https://wecto.nomoredomainsicu.ru"
-            className="about-me__link about-me__link_type_text"
+            className="portfolio__link about-me__link_type_text"
             title="Место"
             target="_blank"
-            rel="noreferrer">Одностраничное приложение
-            <span className="about-me__link about-me__link_type_icon">↗</span></a>
+            rel="noreferrer">Одностраничное приложение</a>
         </li>
       </ul>
-    </>
+    </article>
   )
 }
 
